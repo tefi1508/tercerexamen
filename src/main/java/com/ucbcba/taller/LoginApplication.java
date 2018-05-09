32172afae74ab398dd567228cc951d330a084e10
oskarin44 <Pliniojoao95>
2018-05-09 16:12:51 -0400

    Login

--- src/main/java/com/ucbcba/taller/LoginApplication.java
+++ src/main/java/com/ucbcba/taller/LoginApplication.java
@@ -3,7 +3,7 @@ package com.ucbcba.taller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={"com.ucbcba.taller.services.RestaurantService","com.service.application"})
+@SpringBootApplication
 public class LoginApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ucbcba/taller/repositories/RestaurantRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ucbcba.taller.repositories;
-
-import com.ucbcba.taller.entities.Restaurant;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.transaction.annotation.Transactional;
-
-public interface RestaurantRepository extends CrudRepository<Restaurant,Integer> {
-}
